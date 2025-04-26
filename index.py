303a80ba6ce3d8f766e05c658f8626594a82e65e
Carl Ferros Blancaflor <blancaflorcarlferros@gmail.com>
2025-04-26 21:37:01 +0800

    add: icon photo, title

---
+++ index.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("400x600")
+window.title("Simple Calculator")
+icon = PhotoImage(file="icon.png")
+window.iconphoto(True, icon)
+
+window.mainloop()
